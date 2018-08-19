e97add8a3a74209a3c4581ba9d3460cefd03ed63
ppurchala <pawel.purchala@gmail.com>
2018-08-19 21:45:38 +0200

    pierwsze dane testowe w tabelce person

---
+++ src/main/resources/db/migration/V20180819212630__populate_person.sql
@@ -0,0 +1,4 @@
+INSERT INTO person (id, firstname, lastname) VALUES (1, 'Lolek', 'Lolkowy');
+INSERT INTO person (id, firstname, lastname) VALUES (2, 'Ambrozy', 'Kleks');
+INSERT INTO person (id, firstname, lastname) VALUES (3, 'Jurek', 'Burek');
+INSERT INTO person (id, firstname, lastname) VALUES (4, 'Johny', 'Bravo');
